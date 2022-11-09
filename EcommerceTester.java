c9c49c0546262265f951bd3c21cdc4ff979da4e5
spandana620 <spandanasr.xworkz@gmail.com>
2022-11-09 23:23:59 +0530

    commiting the inheritance related files

--- EcommerceTester.java
+++ EcommerceTester.java
@@ -1,19 +1,15 @@
-class EcommerceTester{
-  public static void main(String a[]){
-  
-Ecommerce.setPricingId(1234);
- Ecommerce.setSku("sku123");	
- Ecommerce.setPrice(2000);
-  Ecommerce.setCurrency("2000 dollar");
-  
-  System.out.println(Ecommerce.getPricingId());
-    System.out.println(Ecommerce.getSku());
-	  System.out.println( Ecommerce.getPrice());
-	    System.out.println( Ecommerce.getCurrency());
-		
-  
-  
-  }
+package com.xworkz.inheritanceapp;
+
+import com.xworkz.inheritanceapp.ECommerce.Ecommerce;
+import com.xworkz.inheritanceapp.ECommerce.FlipKart;
 
 
+
+public class EcommerceTester {
+	public static void main(String a[]){
+		Ecommerce com=new Ecommerce();
+		com.getItems();
+		FlipKart ref=new FlipKart();
+		ref.getItems();
+}
 }
--- FestivalTester.java
+++ FestivalTester.java
@@ -1,42 +1,19 @@
-class FestivalTester{
- public static void main(String a[]){
-  Festival fest=new Festival();
-  fest.festivalName="Deepavali";
-  fest.date="20-oct-2022";
-  fest.relegion="Hindu";
+package com.xworkz.inheritanceapp;
 
-	System.out.println(fest.festivalName + " "+fest.date+" " +fest.relegion);
-	
-	Festival fest1=new Festival();
-  fest1.festivalName="Chirstmas";
-  fest1.date="25-dec-2022";
-  fest1.relegion="Christian";
+import com.xworkz.inheritanceapp.festival.Deepavali;
+import com.xworkz.inheritanceapp.festival.Festival;
 
-	System.out.println(fest1.festivalName + " "+fest1.date+" " +fest1.relegion);
+public class FestivalTester {
 
-	Festival fest2=new Festival();
-  fest2.festivalName="Ganesha Chaturthi";
-  fest2.date="22-aug-2022";
-  fest2.relegion="Hindu";
+		public static void main(String a[]){
+			
+			Festival fs=new Festival();
+			fs.fst();
+			
+			Deepavali de=new Deepavali();
+			de.fst();
+		}
 
-	System.out.println(fest2.festivalName + " "+fest2.date+" " +fest2.relegion);
-  
-  Festival fest3=new Festival();
-  fest3.festivalName="Ugadhi";
-  fest3.date="23-mar-2022";
-  fest3.relegion="Hindu";
+	}
 
-	System.out.println(fest3.festivalName + " "+fest3.date+" " +fest3.relegion);
-	
-  
- Festival fest4=new Festival();
-  fest4.festivalName="Ramzan";
-  fest4.date="16-june-2022";
-  fest4.relegion="Muslim";
 
-	System.out.println(fest4.festivalName + " "+fest4.date+" " +fest4.relegion);
-  
-
- }
-
-}
